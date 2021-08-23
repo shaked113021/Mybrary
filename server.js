1fe3e8a9eaf48ea9e1f1ef7fcc350b561e39bad8
Shaked Cohen <27896110+shaked113021@users.noreply.github.com>
2021-08-23 21:18:34 +0300

    finished setting up server.js

--- server.js
+++ server.js
@@ -2,5 +2,14 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 
+const port = 5000;
+
 app.set('view engine', 'ejs');
-app.set('views', `${__dirname}/views`)
+app.set('views', `${__dirname}/views`);
+app.set('layout', 'layouts/layout');
+
+app.use(expressLayouts);
+
+app.use(express.static('public'));
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
