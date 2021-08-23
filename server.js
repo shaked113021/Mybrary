058004089d3db7ca0ec30c92f4c03c1447e1c7cd
Shaked Cohen <27896110+shaked113021@users.noreply.github.com>
2021-08-23 21:47:26 +0300

    added .env and setup database

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+if (process.env.NODE_ENV !== 'production') {
+  const fs = require('fs');
+  const dotenv = require('dotenv');
+  dotenv.config(dotenv.parse(fs.readFileSync(`${__dirname}/.env`, { encoding: 'utf-8' })));
+}
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
 
@@ -13,6 +19,12 @@ app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 
 app.use(express.static('public'));
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+
+db.on('error', (err) => console.error(err));
+db.once('open', () => console.log('Connected to DB'));
 
 app.use('/', indexRouter);
 
