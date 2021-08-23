4b9eb45c6ae09d45bae58d950bde42ba77b4b77c
Shaked Cohen <27896110+shaked113021@users.noreply.github.com>
2021-08-23 23:30:00 +0300

    handled create author

--- routes/authors.js
+++ routes/authors.js
@@ -14,6 +14,20 @@ router.get('/new', (req, res) => {
 
 // Create author
 router.post('/', (req, res) => {
-  res.send('Create');
+  const author = new Author({
+    name: req.body.name
+  });
+
+  author.save((err, newAuthor) => {
+    if (err) {
+      res.render('authors/new', {
+        author: author,
+        errorMessage: 'Error Creating author'
+      })
+      } else {
+        // res.redirect(`/authors/${newAuthor.id}`);
+        res.redirect('authors');
+    }
+  });
 });
 module.exports = router;
